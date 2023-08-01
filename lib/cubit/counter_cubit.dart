2f180745dda14b73a47e69ee00f708e1d60e19b0
Sina Hajipour <dev.sinahajipour@gmail.com>
2023-08-01 21:35:20 +0330

    implement all the functions that we need to implement on counter state

---
+++ lib/cubit/counter_cubit.dart
@@ -0,0 +1,11 @@
+import 'package:bloc/bloc.dart';
+
+part 'counter_state.dart';
+
+class CounterCubit extends Cubit<CounterState> {
+  // set the initial state of the cubit
+  CounterCubit() : super(CounterState(counterValue: 0));
+// implement all the functions that we have
+  void increament() => emit(CounterState(counterValue: state.counterValue + 1));
+  void decreament() => emit(CounterState(counterValue: state.counterValue - 1));
+}
