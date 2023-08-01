36ca268b83628857427f280dba98197b09953ed0
Sina Hajipour <dev.sinahajipour@gmail.com>
2023-08-01 21:31:36 +0330

    create a blue print of all the possible states

---
+++ lib/cubit/counter_state.dart
@@ -0,0 +1,7 @@
+part of 'counter_cubit.dart';
+
+// this class is a blue print of all the possible states that we have in our cubit
+class CounterState {
+  final int counterValue;
+  CounterState({required this.counterValue});
+}
