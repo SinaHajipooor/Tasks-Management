8c97b22470e751e459314e7c7ffb36157f7876c9
Sina Hajipour <dev.sinahajipour@gmail.com>
2023-08-02 12:44:56 +0330

    add another feild to cubit state to know if it was increamenting or decreamenting

--- lib/cubit/counter_cubit.dart
+++ lib/cubit/counter_cubit.dart
@@ -4,8 +4,8 @@ part 'counter_state.dart';
 
 class CounterCubit extends Cubit<CounterState> {
   // set the initial state of the cubit
-  CounterCubit() : super(CounterState(counterValue: 0));
+  CounterCubit() : super(CounterState(counterValue: 0, wasIncreamented: false));
 // implement all the functions that we have
-  void increament() => emit(CounterState(counterValue: state.counterValue + 1));
-  void decreament() => emit(CounterState(counterValue: state.counterValue - 1));
+  void increament() => emit(CounterState(counterValue: state.counterValue + 1, wasIncreamented: true));
+  void decreament() => emit(CounterState(counterValue: state.counterValue - 1, wasIncreamented: false));
 }
--- lib/cubit/counter_state.dart
+++ lib/cubit/counter_state.dart
@@ -3,5 +3,6 @@ part of 'counter_cubit.dart';
 // this class is a blue print of all the possible states that we have in our cubit
 class CounterState {
   final int counterValue;
-  CounterState({required this.counterValue});
+  final bool wasIncreamented;
+  CounterState({required this.counterValue, required this.wasIncreamented});
 }
