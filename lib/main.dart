8f0d1e506e69151867926488a216860713e39438
Sina Hajipour <dev.sinahajipour@gmail.com>
2023-08-03 15:18:06 +0330

    remove the implemention of cubit in our main file

--- lib/main.dart
+++ lib/main.dart
@@ -37,49 +37,38 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    final blocProvider = BlocProvider.of<CounterCubit>(context);
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: BlocConsumer<CounterCubit, CounterState>(
-        listener: (context, state) {
-          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-            content: Text(state.wasIncreamented ? 'Increamented' : 'decreamented'),
-            duration: const Duration(microseconds: 200),
-          ));
-        },
-        builder: (context, state) {
-          return Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text(
-                  'COUNTER VALUE :  ${state.counterValue.toString()}',
-                  style: Theme.of(context).textTheme.headlineMedium,
-                ),
-                const SizedBox(height: 24),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    FloatingActionButton(
-                      tooltip: 'Decreament',
-                      onPressed: () => blocProvider.decreament(),
-                      child: const Icon(Icons.remove),
-                    ),
-                    const SizedBox(width: 25),
-                    FloatingActionButton(
-                      tooltip: 'Increament',
-                      onPressed: () => blocProvider.increament(),
-                      child: const Icon(Icons.add),
-                    ),
-                  ],
-                )
-              ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'COUNTER VALUE :  ',
+              style: Theme.of(context).textTheme.headlineMedium,
             ),
-          );
-        },
+            const SizedBox(height: 24),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                FloatingActionButton(
+                  tooltip: 'Decreament',
+                  onPressed: () {},
+                  child: const Icon(Icons.remove),
+                ),
+                const SizedBox(width: 25),
+                FloatingActionButton(
+                  tooltip: 'Increament',
+                  onPressed: () {},
+                  child: const Icon(Icons.add),
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
