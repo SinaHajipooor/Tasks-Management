4796e224b5d35d9f4a79ac0d696f5c33642779de
Sina Hajipour <dev.sinahajipour@gmail.com>
2023-08-07 09:46:27 +0330

    implement the logic of add task event

--- lib/blocs/bloc/tasks_bloc.dart
+++ lib/blocs/bloc/tasks_bloc.dart
@@ -6,9 +6,25 @@ part 'tasks_event.dart';
 part 'tasks_state.dart';
 
 class TasksBloc extends Bloc<TasksEvent, TasksState> {
-  TasksBloc() : super(TasksInitial()) {
-    on<TasksEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+  // define the initial state of task
+  TasksBloc() : super(const TasksState()) {
+    // now define the logic that we have on each event
+    on<AddTask>(_onAddTask);
+    on<UpdateTask>(_onUpdateTask);
+    on<DeleteTask>(_onDeleteTask);
   }
+
+// add task logic
+  void _onAddTask(AddTask event, Emitter<TasksState> emit) {
+    final state = this.state;
+    emit(TasksState(
+      allTasks: List.from(state.allTasks)..add(event.task),
+    ));
+  }
+
+  // update task logic
+  void _onUpdateTask(UpdateTask event, Emitter<TasksState> emit) {}
+
+  // delete task
+  void _onDeleteTask(DeleteTask event, Emitter<TasksState> emit) {}
 }
--- lib/blocs/bloc/tasks_state.dart
+++ lib/blocs/bloc/tasks_state.dart
@@ -1,11 +1,9 @@
 part of 'tasks_bloc.dart';
 
-sealed class TasksState extends Equatable {
+class TasksState extends Equatable {
   final List<Task> allTasks;
   const TasksState({this.allTasks = const <Task>[]});
 
   @override
   List<Object> get props => [allTasks];
 }
-
-final class TasksInitial extends TasksState {}
