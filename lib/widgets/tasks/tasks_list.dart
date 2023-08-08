a5d71aa74a02af67d73ba220619bba65ac29c629
Sina Hajipour <dev.sinahajipour@gmail.com>
2023-08-08 12:49:04 +0330

    change the physics of the list view

--- lib/widgets/tasks/tasks_list.dart
+++ lib/widgets/tasks/tasks_list.dart
@@ -12,6 +12,7 @@ class TasksList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
+      physics: const BouncingScrollPhysics(),
       itemCount: tasksList.length,
       itemBuilder: (context, index) {
         var task = tasksList[index];
